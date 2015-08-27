47aa3e74ac82b0e5b78706898824c69cae08c6d7
tdarlington <darlingtont@gmail.com>
2015-08-27 08:08:55 -0400

    First

---
+++ Assets/Scripts/BearBot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearBot : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Hooray()
+	{
+		animator.SetTrigger ("Hooray");
+	}
+	
+	public void Hello()
+	{
+		animator.SetTrigger ("Hello");
+	}
+	
+	public void SlideLeft()
+	{
+		animator.SetTrigger ("SlideLeft");
+	}
+
+}
